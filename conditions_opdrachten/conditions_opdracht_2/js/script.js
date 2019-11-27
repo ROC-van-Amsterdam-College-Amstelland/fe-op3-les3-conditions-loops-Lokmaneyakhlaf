24f45aecb1b651274f370fa775340a726312d5e0
lokmane yakhlaf <2114693@talnet.nl>
2019-11-27 11:30:41 +0100

    opdracht 1 form

--- conditions_opdrachten/conditions_opdracht_2/js/script.js
+++ conditions_opdrachten/conditions_opdracht_2/js/script.js
@@ -1,1 +1,16 @@
 //javascript code
+
+function checkSaldo(){
+    var geldOpname = document.getElementById('geldOpname').value;
+
+    alert(geldOpname);
+
+    var banksaldo = 1000;
+
+    var restTotaal = 1000 - geldOpname
+
+    if (restTotaal < 25) {
+        alert("Sorry, je hebt te weinig saldo!");
+    }
+}                                                                           
+
--- loops_opdrachten/loops_opdracht_1/js/script.js
+++ loops_opdrachten/loops_opdracht_1/js/script.js
@@ -1,1 +1,15 @@
 //javascript code
+
+/*
+
+for(statement 1; statement 2; statement 3){
+
+}
+teller++
+teller = teller + 1;
+teller += 1;
+*/
+
+for(var teller  = 1 ; teller <= 5 ; teller++){
+    console.log(teller)
+}
