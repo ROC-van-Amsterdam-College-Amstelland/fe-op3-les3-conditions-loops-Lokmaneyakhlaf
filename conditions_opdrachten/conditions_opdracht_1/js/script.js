68de74442541f81599cc922db18995de63b29035
lokmane yakhlaf <2114693@talnet.nl>
2019-12-03 11:37:49 +0100

    begin gemaakt aan opdracht 1 van conditions

--- conditions_opdrachten/conditions_opdracht_1/js/script.js
+++ conditions_opdrachten/conditions_opdracht_1/js/script.js
@@ -1,1 +1,10 @@
 //javascript code
+
+var banksaldo = 24; //huidige banksado
+
+if (banksaldo < 25) {
+    //alert("sorry, je hebt te weinig saldo");
+
+    document.getElementById('tekst').innerHTML =
+    "sorry, je hebt te weinig saldo!";
+}
