cdb5cd4937f010e955c65d1e043f5d647ca684f0
lokmane yakhlaf <2114693@talnet.nl>
2019-12-05 14:56:08 +0100

    opdracht 5

--- loops_opdrachten/loops_opdracht_5/js/script.js
+++ loops_opdrachten/loops_opdracht_5/js/script.js
@@ -1,1 +1,16 @@
-//javascript code
+
+
+for( var teller = 1  ; teller <= 20  ; teller++) {
+    console.log(teller);
+    document.getElementById("resultaat").innerHTML += "<h6>" + teller + "</h6>";
+    console.log(teller % 2);
+
+    if(teller % 2 == 0) {
+        document.getElementById("resultaat").innerHTML += "<h6>" + teller + "<h6>";
+    }
+    else{
+        console.log(teller);
+    }
+
+
+}
